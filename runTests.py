8b4302aed0d60b33651f0ce10cfa9b382d359d6a
Mikkel Levisen <mikkel.levisen@gmail.com>
2015-11-12 15:32:12 +0100

    runTests.py basic structure up and running

--- runTests.py
+++ runTests.py
@@ -8,27 +8,31 @@ def var_dump(*x):
         print("---------------")
 
 
-#def testTree(folder):
-for dirname, dirnames, filenames in os.walk('tests'):
-    for subdirname in dirnames:
-        for sub_dirname,sub_dirnames,sub_filenames in os.walk(os.path.join(dirname,subdirname)):
-            for f in sub_filenames:
-                testData = f.split("_")
-                n = None
-                d = None
-                r = None
-                s = None
-                for i in range(len(testData)):
+def testTree(folder):
+    for dirname, dirnames, filenames in os.walk('tests'):
+        for subdirname in dirnames:
+            for sub_dirname,sub_dirnames,sub_filenames in os.walk(os.path.join(dirname,subdirname)):
+                for f in sub_filenames:
+                    testData = f.split("_")
+                    n = None
+                    d = None
+                    r = None
+                    s = None
+                    for i in range(len(testData)):
 
-                    if testData[i] == "n":
-                       n = testData[i+1]
-                    elif testData[i] == "d":
-                       d = testData[i+1]
-                    elif testData[i] == "r":
-                       r = testData[i+1]
-                    elif testData[i] == "s":
-                       s = testData[i+1].split(".")[0]
-                parseTestFile()
+                        if testData[i] == "n":
+                           n = testData[i+1]
+                        elif testData[i] == "d":
+                           d = testData[i+1]
+                        elif testData[i] == "r":
+                           r = testData[i+1]
+                        elif testData[i] == "s":
+                           s = testData[i+1].split(".")[0]
 
-            #print(os.path.join(dirname, subdirname))
+                    os.chdir(folder)
 
+                    command = "lua test.lua " + os.path.join("..",dirname, subdirname,f) + " -R"
+                    os.system(command)
+                    os.chdir("..")
+
+testTree("rangeTree")
