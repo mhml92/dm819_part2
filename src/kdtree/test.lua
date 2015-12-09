65b03a682bd8e9467064c93045060cc38d2be03c
Jesper Lund <jesper.beltoft.lund@gmail.com>
2015-12-09 11:33:31 +0100

    fixed tests

--- src/kdtree/test.lua
+++ src/kdtree/test.lua
@@ -174,7 +174,7 @@ else
          print("kdTree\t"..fileName.."\t"..measures.n.."\t"..v.outSize.."\t".. tostring(v.correct) .."\t"..measures.build_t2-measures.build_t1.."\t"..v.t2-v.t1,gb/1024 .. "\t" .. #P[1])
       end
    else
-      local rt = kd.BUILDKDTREE(P)
+      local rt,gb = kd.BUILDKDTREE(P, 0, #P[1])
       local res = {}
       for k,r in ipairs(R) do
          local result = kd.INITKDSEARCH(rt, r)
